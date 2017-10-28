1b6ca532264036dae3ef1d233039814ac8003639
tsd832 <32853441+tsd832@users.noreply.github.com>
2017-10-28 00:39:51 -0500

    pre-launch v1.1.6

--- JQUERY/scripts.js
+++ JQUERY/scripts.js
@@ -10,3 +10,11 @@ $(document).ready(function()
 {
   $("#button").click(notify);
 })
+
+
+$(window).on('resize', function(event){
+	var windowSize = $(window).width(); // Could've done $(this).width()
+    if(windowSize > 960){
+    	$('#main-menu').css('display', 'inline');
+    }
+});
